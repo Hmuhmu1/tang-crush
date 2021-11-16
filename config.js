d8412e2a9d0092515cf6205353dda502ed759caf
Hmuhmu1 <94427986+Hmuhmu1@users.noreply.github.com>
2021-11-16 12:33:17 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,19 +1,19 @@
 const CONFIG = {
-    introTitle: 'Babe à!',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
-    btnIntro: 'hihi',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Vẫn cứ là thích anh <33',
-    btnNo: 'Không, Anh trai à :3',
-    question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
+    introTitle: 'lo condy!',
+    introDesc: `trái đất cứ lạ thường
+    Mà sao m cứ đi nhầm đường
+    Lạc vào tim kao lẻ loi
+    Đằng sau chữ yêu đây là ghét`,
+    btnIntro: 'sớ',
+    title: 'Phải chăng m đã ghét ngay từ cái nhìn đầu tiên 😙',
+    desc: 'Phải chăng m đã khum ưa ngay từ lúc thấy nụ cười ấy ',
+    btnYes: 'Vẫn cứ là ghét t :))',
+    btnNo: 'ko, ghét m mà :3',
+    question:'Trên thế giới hơn 7 tỉ người mà sao m lại ko CHỈ BÀI CKO T <3',
     btnReply: 'Gửi cho anh <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h anh qua đón nhé công chúa.',
+    reply: 'chỉ thì thì mà không chỉ thì chỉ <33333333',
+    mess: 't biết mà:((. dỗi nhiều nhiều 😘😘',
+    messDesc: 'mốt chỉ bài  nka condy.',
     btnAccept: 'Okiiiii lun <3',
-    messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
+    faceLink://link face của t(do chỉ bài0.: https://www.facebook/profile.php?id=100058690996292
 }
